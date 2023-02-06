1ff71eaf6660354f6e4e387257689533cbce6489
Alyssa <alyssa@kvasl.com>
2023-02-07 03:07:44 +1300

    Navbar is now resposnsive started on bottom nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,48 +1,33 @@
 import React from 'react'
-import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography, BottomNavigation, BottomNavigationAction } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography, BottomNavigation, BottomNavigationAction, useTheme, useMediaQuery } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
 import { Link, useLocation } from 'react-router-dom';
 
-import logo from '../../assets/Asset1.png'
+import logo from '../../assets/Asset7.png'
+import mobileLogo from '../../assets/Asset10.png'
 import useStyles from './styles'
 
 const Navbar = ({totalItems}) => {
 
   const classes = useStyles();
   const location = useLocation();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
-  return (
-    <>
-        {/* <AppBar position='fixed' className={classes.appBar} color='inherit'>
-            <Toolbar>
-                <Typography component={Link} to="/" variant='h6' className={classes.title} color='inherit'>
-                    <img src={logo} alt='Commerce.js' height='25px' className={classes.image} />
-                </Typography>
-                <div className={classes.grow}/>
-                {location.pathname === '/' && (
-                  <div className={classes.button}>
-                    <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
-                      <Badge badgeContent={totalItems} color="secondary">
-                        <ShoppingCart />
-                      </Badge>
-                    </IconButton>
-                  </div>
-                  )}
-            </Toolbar>
-        </AppBar> */}
-
-        {/* <AppBar>
+  const dsktpNav = () => {
+    return(
+      <AppBar className={classes.appBar} elevation={0} position="fixed" >
           <Toolbar>
             <Typography component={Link} to="/" >
-              <img src={logo} alt='Frank Albie' />
+              <img className={classes.logo} src={logo} alt='Frank Albie' />
             </Typography>
-            <Typography component={Link} to="/">Shop</Typography>
-            <Typography component={Link} to="/">About</Typography>
-            <Typography component={Link} to="/">Contact</Typography>
+            <Typography component={Link} to="/" className={classes.links}>Shop</Typography>
+            <Typography component={Link} to="/" className={classes.links}>About</Typography>
+            <Typography component={Link} to="/" className={classes.links}>Contact</Typography>
                 {location.pathname === '/' && (
-                  <div>
-                    <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
-                      <Badge badgeContent={totalItems} color="secondary">
+                  <div className={classes.cart}>
+                    <IconButton component={Link} to="/cart" aria-label="Show cart items" color="#354721">
+                      <Badge badgeContent={totalItems} color="#F7DACC">
                         <ShoppingCart />
                       </Badge>
                     </IconButton>
@@ -50,15 +35,25 @@ const Navbar = ({totalItems}) => {
                   )}
 
           </Toolbar>
-        </AppBar> */}
+        </AppBar>
+    )
+  };
 
-        {/* <BottomNavigation id='devicesNav'>
+  const mobileNav = () => {
+    return(
+      <BottomNavigation id='devicesNav'>
           <BottomNavigationAction component={Link} to='/' label="Shop" icon={<ShoppingCart />} />
           <BottomNavigationAction component={Link} to='/' label="About" icon={<ShoppingCart />} />
-          <BottomNavigationAction component={Link} to='/' label="Home" icon={<ShoppingCart />} />
+          <BottomNavigationAction component={Link} to='/' label="Home" icon={<img src={mobileLogo} alt='Frank Albie' />} />
           <BottomNavigationAction component={Link} to='/' label="Contact" icon={<ShoppingCart />} />
           <BottomNavigationAction component={Link} to='/' label="Cart" icon={<ShoppingCart />} />
-        </BottomNavigation> */}
+        </BottomNavigation>
+    )
+  }
+
+  return (
+    <>
+      {isMobile ? (mobileNav()) : (dsktpNav())}
     </>
   )
 }
--- src/components/products/styles.js
+++ src/components/products/styles.js
@@ -1,7 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
-  toolbar: theme.mixins.toolbar,
   container: {
     padding: '0 6%',
   },
